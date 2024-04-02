c7f34f9e258ddd4d4ccf769e6ddb13b50170cc23
dhuydo <ncdhuy.jul04@gmail.com>
2024-04-02 14:53:18 +0700

    Add files

---
+++ app.py
@@ -0,0 +1,40 @@
+#import libraries
+import numpy as np
+import pandas as pd
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+#Initialize the flask App
+app = Flask(__name__)
+ohe, ss, model = pickle.load(open('model_diabetes.pkl', 'rb'))
+
+#default page of our web-app
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#To use the predict button in our web-app
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    ls = [x for x in request.form.values()]
+    int_features = []
+    for i in ls:
+        try:
+            u = int(i)
+            int_features.append(u)
+        except:
+            int_features.append(i)
+    catcol = ohe.transform(pd.DataFrame(int_features[:7]).T).A
+    numcol = ss.transform(pd.DataFrame(int_features[7:]).T)
+    x_train = np.concatenate([catcol, numcol], axis=1)
+
+    prediction = model.predict(x_train)
+
+    output = int(np.exp(prediction[0]))
+
+    return render_template('index.html', prediction_cost='Chi phí điều trị một năm của bệnh nhân: {:,} VND'.format(output).replace(',','.'))
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=1003)
