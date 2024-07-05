ea337a66a5fc004565d9affcda4de08c585e8bae
dhuydo <ncdhuy.d17@d17gmail.com>
2024-07-05 20:42:52 +0700

    Change the model at 5 Jul

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 
 #Initialize the flask App
 app = Flask(__name__)
-ohe, ss, model = pickle.load(open('model_diabetes.pkl', 'rb'))
+pipeline = pickle.load(open('pipeline.pkl', 'rb'))
 
 #default page of our web-app
 @app.route('/')
@@ -20,18 +20,21 @@ def predict():
     For rendering results on HTML GUI
     '''
     ls = [x for x in request.form.values()]
-    int_features = []
-    for i in ls:
-        try:
-            u = int(i)
-            int_features.append(u)
-        except:
-            int_features.append(i)
-    catcol = ohe.transform(pd.DataFrame(int_features[:7]).T).A
-    numcol = ss.transform(pd.DataFrame(int_features[7:]).T)
-    x_train = np.concatenate([catcol, numcol], axis=1)
-
-    prediction = model.predict(x_train)
+    # int_features = []
+    # for i in ls:
+    #     try:
+    #         u = int(i)
+    #         int_features.append(u)
+    #     except:
+    #         int_features.append(i)
+    # catcol = ohe.transform(pd.DataFrame(int_features[:7]).T).A
+    # numcol = ss.transform(pd.DataFrame(int_features[7:]).T)
+    # x_train = np.concatenate([catcol, numcol], axis=1)
+    cols = ['Ky_QT_adjust','GIOI_TINH','Tuổi','nhomTuoi','regions','HI','insurance','hang','tuyen','MA_LYDO_VVIEN','MA_LOAI_KCB','Số ngày điều trị','checkComorbidities',
+            'comorbidities','soBienChung_cat','loaiBienChung','diemBienChung_cat']
+    x = pd.DataFrame([ls], columns=cols)
+    # return ls
+    prediction = pipeline.predict(x)
 
     output = int(np.exp(prediction[0]))
 
